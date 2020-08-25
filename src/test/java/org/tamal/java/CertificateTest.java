fc8a23782e0fea7129a7e2ce319f6571b50bee98
Tamal Nath <tamal.nath@gmail.com>
2020-08-26 00:42:26 +0530

    Fixed CA pathlen error.

--- src/test/java/org/tamal/java/CertificateTest.java
+++ src/test/java/org/tamal/java/CertificateTest.java
@@ -21,13 +21,15 @@ import java.security.cert.TrustAnchor;
 import java.security.cert.X509CertSelector;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 public class CertificateTest {
     private Path dir;
@@ -35,23 +37,24 @@ public class CertificateTest {
     @BeforeClass
     void init() throws IOException, InterruptedException {
         dir = Files.createTempDirectory("certificate-");
+        System.out.println(dir);
         Path script = dir.resolve("generate-certificates.sh");
         Files.copy(getClass().getResourceAsStream("/generate-certificates.sh"), script);
-        Process process = new ProcessBuilder("bash", script.toString()).directory(dir.toFile()).inheritIO().start();
+        Process process = new ProcessBuilder("bash", "-x", script.toString()).directory(dir.toFile()).inheritIO().start();
         assertEquals(process.waitFor(), 0);
     }
 
     @Test
     void testValidCertificateChain() throws IOException, GeneralSecurityException {
-        verifyCertificateChain(dir.resolve("rootca.crt"), dir.resolve("server.crt"), dir.resolve("intermediate0.crt"));
+        validate(dir.resolve("server.crt"), dir.resolve("intermediate0.crt"), dir.resolve("rootca.crt"));
     }
 
     @Test(expectedExceptions = CertPathValidatorException.class)
     void testCertificateChainInWrongOrder() throws IOException, GeneralSecurityException {
         try {
-            verifyCertificateChain(dir.resolve("rootca.crt"), dir.resolve("intermediate0.crt"), dir.resolve("server.crt"));
+            validate(dir.resolve("intermediate0.crt"), dir.resolve("server.crt"), dir.resolve("rootca.crt"));
         } catch (CertPathValidatorException e) {
-            assertEquals(e.getReason(), PKIXReason.NO_TRUST_ANCHOR);
+             assertEquals(e.getReason(), PKIXReason.NO_TRUST_ANCHOR);
             throw e;
         }
     }
@@ -59,7 +62,7 @@ public class CertificateTest {
     @Test(expectedExceptions = CertPathValidatorException.class)
     void testCertificateMissingIntermediateCA() throws IOException, GeneralSecurityException {
         try {
-            verifyCertificateChain(dir.resolve("rootca.crt"), dir.resolve("server.crt"));
+            validate(dir.resolve("server.crt"), dir.resolve("rootca.crt"));
         } catch (CertPathValidatorException e) {
             assertEquals(e.getReason(), PKIXReason.NO_TRUST_ANCHOR);
             throw e;
@@ -69,61 +72,36 @@ public class CertificateTest {
     @Test(expectedExceptions = CertPathValidatorException.class)
     void testCertificatePathLength() throws IOException, GeneralSecurityException {
         try {
-            verifyCertificateChain(dir.resolve("rootca0.crt"), dir.resolve("server.crt"), dir.resolve("intermediate.crt"));
+            validate(dir.resolve("server0.crt"), dir.resolve("intermediate.crt"), dir.resolve("intermediate0.crt"), dir.resolve("rootca.crt"));
         } catch (CertPathValidatorException e) {
-            assertEquals(e.getReason(), PKIXReason.NAME_CHAINING);
+            assertEquals(e.getReason(), PKIXReason.PATH_TOO_LONG);
             throw e;
         }
     }
 
-    @Test
-    void testCertificateNoIssuerExtension() throws IOException, GeneralSecurityException {
-        verifyCertificateChain(dir.resolve("rootca-no-ext.crt"), dir.resolve("server-no-issuer-ext.crt"));
-    }
-
-    @Test
-    void testCertificateIssuerNotCA() throws IOException, GeneralSecurityException {
-        verifyCertificateChain(dir.resolve("not-ca.crt"), dir.resolve("server-issuer-not-ca.crt"));
-    }
-
-    @Test
-    void testCertificateWrongKeyUsage() throws IOException, GeneralSecurityException {
-        verifyCertificateChain(dir.resolve("rootca-wrong-keyUsage.crt"), dir.resolve("wrong-issuer-keyUsage.crt"));
-    }
-
-    @Test
-    void testCertificateWrongExtendedKeyUsage() throws IOException, GeneralSecurityException {
-        verifyCertificateChain(dir.resolve("rootca-wrong-extendedKeyUsage.crt"), dir.resolve("wrong-issuer-extendedKeyUsage.crt"));
-    }
-
     /**
      * Verifies certificate chain against a trusted certificate.
-     * @param trustedCert the trusted certificate or the Root CA
-     * @param certPaths the certificate paths
+     * @param certificates the certificate chain in server-intermediate-root order
      * @return result of the validation algorithm
      * @throws IOException if the file cannot be read
      * @throws GeneralSecurityException if the certificate cannot be parsed or the validation fails
      */
-    private CertPathValidatorResult verifyCertificateChain(Path trustedCert, Path... certPaths) throws IOException, GeneralSecurityException {
+    private CertPathValidatorResult validate(Path... certificates) throws IOException, GeneralSecurityException {
         CertificateFactory factory = CertificateFactory.getInstance("X.509");
-        X509Certificate rootCA;
-        try (InputStream inputStream = new FileInputStream(trustedCert.toFile())) {
-            Collection<? extends Certificate> certs = factory.generateCertificates(inputStream);
-            assertEquals(certs.size(), 1);
-            rootCA = (X509Certificate) certs.iterator().next();
-        }
-        Set<TrustAnchor> anchors = Collections.singleton(new TrustAnchor(rootCA, null));
-        List<Certificate> certificates = new ArrayList<>();
-        for (Path certPath : certPaths) {
+        List<Certificate> certs = new ArrayList<>();
+        for (Path certPath : certificates) {
             try (InputStream inputStream = new FileInputStream(certPath.toFile())) {
-                certificates.addAll(factory.generateCertificates(inputStream));
+                certs.addAll(factory.generateCertificates(inputStream));
             }
         }
-        CertPath certPath = factory.generateCertPath(certificates);
-        X509CertSelector targetConstraints = new X509CertSelector();
-        targetConstraints.setCertificateValid(new Date());
+        assertTrue(certs.size() > 1);
+        X509Certificate rootCA = (X509Certificate) certs.remove(certs.size() - 1);
+        Set<TrustAnchor> anchors = Collections.singleton(new TrustAnchor(rootCA, null));
+        CertPath certPath = factory.generateCertPath(certs);
+        X509CertSelector x509CertSelector = new X509CertSelector();
+        x509CertSelector.setCertificateValid(new Date());
         PKIXParameters pkixParameters = new PKIXParameters(anchors);
-        pkixParameters.setTargetCertConstraints(targetConstraints);
+        pkixParameters.setTargetCertConstraints(x509CertSelector);
         pkixParameters.setRevocationEnabled(false);
         CertPathValidator validator = CertPathValidator.getInstance("PKIX");
         return validator.validate(certPath, pkixParameters);
